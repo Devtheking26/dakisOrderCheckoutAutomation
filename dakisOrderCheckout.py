d35ca511669babd131ff25c3198203553df99dc5
Devtheking26 <86381125+Devtheking26@users.noreply.github.com>
2024-03-06 14:10:40 -0700

    Add files via upload

---
+++ dakisOrderCheckout.py
@@ -0,0 +1,73 @@
+import pyautogui
+import time
+import pygetwindow as gw   
+
+def printWindowList():
+    windList =gw.getAllTitles()
+    print(windList)
+
+def getActiveWindow():
+    WindowName ='Dakis Job Downloader'
+    currentWindow = gw.getActiveWindowTitle()
+    if WindowName == currentWindow:
+        return True
+    else:
+        return False
+
+def get_cursor_coords():
+    ans = input("Ready for coordinates? (Y/N) ")
+    while ans.lower() == "y":
+        print("Mouse coordinates:", pyautogui.position())
+        ans = input("Enter another variable? (Y/N)\n")
+
+def open_find():
+    pyautogui.click(x=1371, y=51)
+
+def mark_order():
+    pyautogui.click(x=796, y=43)
+
+def mark_as_done():
+    pyautogui.click(x=819, y=83)
+
+def no_button():
+    pyautogui.click(x=1158, y=551)
+
+def scanner_in():
+    order_num = input("Enter order number: ")
+    print(order_num)
+
+def multi_order_checkout():
+    order_list = []
+    order_number = input("Scan OrderNumber: ")
+    while order_number.lower() != "n":
+        if len(order_number) == 8 and order_number[0] != ".":
+            order_list.append(order_number)
+        else:
+            print("Incorrect order #. Please make sure you are using the correct dakis number and not a bag number.")
+        order_number = input("Next Order Number (If finished press n ): ")
+
+    for order in order_list:
+        checkout_orders(order)
+        #time.sleep(1)
+
+def checkout_orders(order_number):
+    open_find()
+    time.sleep(.2)
+    pyautogui.write(order_number)
+    pyautogui.press('enter')
+    time.sleep(1.4)
+    mark_order() 
+    #time.sleep(0.5)
+    mark_as_done()
+    #time.sleep(3)
+    correctWindow = getActiveWindow()
+    while not correctWindow:
+        correctWindow = getActiveWindow()
+    no_button()
+
+def main():
+    multi_order_checkout()
+    #printWindowList()
+
+if __name__ == '__main__':
+    main()
---
+++ readme.txt
@@ -0,0 +1,10 @@
+How to use DakisOrderheckout.py
+
+
+1. Open Dakis and make sure there are no popups
+2. Run the program
+3. While in the program window follow the prompts
+4. When finished scanning in the Orders type n and press enter
+5. Watch the orders get checked out
+
+**Note: It is very possible weird things will happen if you scan something wrong**
