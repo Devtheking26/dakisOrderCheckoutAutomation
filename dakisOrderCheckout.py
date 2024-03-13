1633283c84c2ab3df58dd22c78e6b54b376787fa
Devtheking26 <86381125+Devtheking26@users.noreply.github.com>
2024-03-13 11:17:59 -0600

    Revert to OG

--- dakisOrderCheckout.py
+++ dakisOrderCheckout.py
@@ -3,7 +3,7 @@ import time
 import pygetwindow as gw   
 
 def printWindowList():
-    windList = gw.getAllTitles()
+    windList =gw.getAllTitles()
     print(windList)
 
 def getActiveWindow():
@@ -12,7 +12,7 @@ def getActiveWindow():
     if WindowName == currentWindow:
         return True
     else:
-        return False      
+        return False
 
 def get_cursor_coords():
     ans = input("Ready for coordinates? (Y/N) ")
@@ -37,11 +37,11 @@ def scanner_in():
     print(order_num)
 
 def multi_order_checkout():
-    order_list = set()
+    order_list = []
     order_number = input("Scan OrderNumber: ")
     while order_number.lower() != "n":
         if len(order_number) == 8 and order_number[0] != ".":
-            order_list.add(order_number)
+            order_list.append(order_number)
         else:
             print("Incorrect order #. Please make sure you are using the correct dakis number and not a bag number.")
         order_number = input("Next Order Number (If finished press n ): ")
