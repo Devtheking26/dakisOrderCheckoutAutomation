38cedc638c78dacdbc2c5bb28f53e07253d512d9
corne <corneliusdevin@gmail.com>
2024-03-13 15:37:13 -0600

    Found and corrected error.

--- dakisOrderCheckout.py
+++ dakisOrderCheckout.py
@@ -6,6 +6,21 @@ def printWindowList():
     windList = gw.getAllTitles()
     print(windList)
 
+def testIO():
+    order_list = set()
+    order_number = input("Scan OrderNumber: ")
+    while order_number.lower() != "n":
+        if len(order_number) == 8 and order_number[0] != "." and int(order_number) > 40000000:
+            order_list.add(order_number)
+        else:
+            print("Incorrect order #. Please make sure you are using the correct dakis number and not a bag number.")
+        order_number = input("Next Order Number (If finished press n ): ")
+
+    for order in order_list:
+        print(order)
+        #time.sleep(1)
+
+
 def getActiveWindow():
     WindowName ='Dakis Job Downloader'
     currentWindow = gw.getActiveWindowTitle()
@@ -66,8 +81,8 @@ def checkout_orders(order_number):
     no_button()
 
 def main():
-    multi_order_checkout()
-    #printWindowList()
+    #multi_order_checkout()
+    testIO()
 
 if __name__ == '__main__':
     main()
